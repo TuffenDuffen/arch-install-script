6fa34662cc1decc186640989937418e4d6f8db4c
TuffenDuffen <50796186+TuffenDuffen@users.noreply.github.com>
2022-06-13 20:24:01 +0200

    chroot workaround

---
+++ chroot.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Basic config
+ln -s /usr/share/zoneinfo/Europe/Stockholm /etc/localtime
+hwclock --systohc
+
+sed -i "s/^#sv_SE.UTF-8 UTF-8/sv_SE.UTF-8 UTF-8/" /etc/locale.gen
+sed -i "s/^#en_GB.UTF-8 UTF-8/en_GB.UTF-8 UTF-8/" /etc/locale.gen
+
+locale-gen
+
+# Creating locale.conf
+cat > /etc/locale.conf << EOF
+LANG=sv_SE.UTF-8
+LANGUAGES=en_GB.UTF-8
+LC_MESSAGES=en_GB.UTF-8
+EOF
+
+# Creating vconsole.conf
+echo "KEYMAP=sv-latin1" > "/etc/vconsole.conf"
+
+# Creating hostname
+echo "tuffen-Mac" > "/etc/hostname"
+
+passwd
--- iso.sh
+++ iso.sh
@@ -19,30 +19,10 @@ swapon /dev/sda2
 # Preinstall packages
 pacstrap /mnt base linux linux-firmware broadcom-wl networkmanager nano sudo man-db man-pages texinfo git base-devel fish
 
+# Prepare the next script
+curl -LJO https://raw.githubusercontent.com/tuffenduffen/arch-install-script/main/chroot.sh --output-dir /mnt
+chmod +x /mnt/chroot.sh
+
 # chroot into new system
 genfstab -U /mnt >> /mnt/etc/fstab
-arch-chroot /mnt
-
-# Basic config
-ln -s /usr/share/zoneinfo/Europe/Stockholm /etc/localtime
-hwclock --systohc
-
-sed -i "s/^#sv_SE.UTF-8 UTF-8/sv_SE.UTF-8 UTF-8/" /etc/locale.gen
-sed -i "s/^#en_GB.UTF-8 UTF-8/en_GB.UTF-8 UTF-8/" /etc/locale.gen
-
-locale-gen
-
-# Creating locale.conf
-cat > /etc/locale.conf << EOF
-LANG=sv_SE.UTF-8
-LANGUAGES=en_GB.UTF-8
-LC_MESSAGES=en_GB.UTF-8
-EOF
-
-# Creating vconsole.conf
-echo "KEYMAP=sv-latin1" > "/etc/vconsole.conf"
-
-# Creating hostname
-echo "tuffen-Mac" > "/etc/hostname"
-
-passwd
+arch-chroot /mnt chroot.sh
