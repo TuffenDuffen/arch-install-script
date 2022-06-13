561424634b7b5476f5440ae4f37810ea02792864
TuffenDuffen <50796186+TuffenDuffen@users.noreply.github.com>
2022-06-13 22:18:47 +0200

    mkinitcpio -P and piping to /dev/null

--- chroot.sh
+++ chroot.sh
@@ -6,7 +6,7 @@ hwclock --systohc
 sed -i "s/^#sv_SE.UTF-8 UTF-8/sv_SE.UTF-8 UTF-8/" /etc/locale.gen
 sed -i "s/^#en_GB.UTF-8 UTF-8/en_GB.UTF-8 UTF-8/" /etc/locale.gen
 
-locale-gen
+locale-gen | /dev/null
 
 # Creating locale.conf
 cat > /etc/locale.conf << EOF
@@ -31,9 +31,10 @@ passwd tuffen
 # Add sudo permission
 sed -i "s/^#%sudo	ALL=(ALL:ALL) ALL/%sudo	ALL=(ALL:ALL) ALL/" /etc/sudoers
 
-# Installing bootloader
-bootctl install
+mkinitcpio -P
 
+# Installing bootloader
 pacman -S intel-ucode
+bootctl install
 
 exit
