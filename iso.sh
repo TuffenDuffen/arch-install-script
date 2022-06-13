0579c460614debca95b655734408eec10f99e9b9
TuffenDuffen <reg_i@wext.se>
2022-06-13 15:55:22 +0200

    partition layout

--- iso.sh
+++ iso.sh
@@ -1,11 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
 # Configure time
 timedatectl set-ntp true
 
-# Create GPT table for 128gB SSD
-parted /dev/sda mklabel gpt \
- mkpart "EFI system partition" fat32 1MiB 512MiB \
- set 1 esp on \
- mkpart "Linux swap" ext4 512MiB 8512MiB\
- mkpart "root partition" ext4 8512MiB 100%
+# Create 512mB EFI, 8gB swap & root partition
+printf 'g\nn\n\n\n+512M\ny\nt\n1\nn\n\n\n+8G\ny\nt\n\n19\nn\n\n\n\ny\nw\n' | fdisk
